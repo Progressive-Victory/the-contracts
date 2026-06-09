4ffefd304f94b43376996e5914ff83f84eac1ff1
sH3llH0und <wyn@progress.win>
2026-06-09 08:24:31 -0600

    unnallabled email

--- src/contracts/requests/ActBlueRequest.ts
+++ src/contracts/requests/ActBlueRequest.ts
@@ -23,7 +23,9 @@ export const zActBlueRequest = z.strictObject({
 		// on whether we manage the donation form or not and choose
 		// to let them. It is unclear from the documentation if this
 		// means this should be optional or not.
-		email: z.string().nullable(),
+
+		// Our system isn't structured to handle a null email. If one is submitted, we should drop the request.
+		email: z.string(),
 		phone: z.string().nullable(),
 	}),
 
