2ade15883f468114a0c44dfbdb82ff96e9b97c66
sH3llH0und <wyn@progress.win>
2026-06-25 09:40:33 -0600

    dont

--- .github/scripts/validate_pr_schema.sh
+++ .github/scripts/validate_pr_schema.sh
@@ -1,1 +1,1 @@
-echo "$1" | grep -q '^change: \(major\|minor\|patch\)$' || (echo "::error::Could Not Find Update SemVer Type" && exit 1)
+echo "$1" | grep -qP '^change: (major|minor|patch)$' || (echo "::error::Could Not Find Update SemVer Type" && exit 1)
