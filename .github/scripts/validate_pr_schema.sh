98ab4e571829fd001a3f9970c30eb9ffea9f4b75
sH3llH0und <wyn@progress.win>
2026-06-25 08:58:26 -0600

    wadkoawd

--- .github/scripts/validate_pr_schema.sh
+++ .github/scripts/validate_pr_schema.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "$1"
+#echo "$1"
 echo "$1" | grep '^change: \(major\|minor\|patch\)$'
+val=$(echo "$1")
+echo "$val"
 val=$(echo "$1" | grep -E '^change: (major|minor|patch)$')
 echo "val: $val"
 #if [ -z "$val" ]; then exit 1; fi
