b5d5d80d5fd0dbb798b0801cd55987c74afeb4e7
sH3llH0und <wyn@progress.win>
2026-06-25 09:47:50 -0600

    penis

--- .github/scripts/validate_pr_schema.sh
+++ .github/scripts/validate_pr_schema.sh
@@ -1,1 +1,1 @@
-echo "$1" | grep -qP '^change: (major|minor|patch)$' || (echo "::error::Could Not Find Update SemVer Type" && exit 1)
+echo "$1" | grep -qP '(^|\s+)change: (major|minor|patch)($|\s+)' || (echo "::error::Could Not Find Update SemVer Type" && exit 1)
