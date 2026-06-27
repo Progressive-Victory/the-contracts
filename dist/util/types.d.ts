176ac80aeebc5fddfcbf0ebf70933d0ba76be082
Automated Publisher <actions@users.noreply.github.com>
2026-06-27 23:35:03 +0000

    Automated publish: Sat Jun 27 23:35:03 UTC 2026 5a80781ac08e507d624e51c29c5d48c55fad39a6

--- dist/util/types.d.ts
+++ dist/util/types.d.ts
@@ -8,5 +8,5 @@ export declare const zIntQuery: z.ZodPipe<z.ZodPipe<z.ZodOptional<z.ZodString>, 
 export declare const zStringQuery: z.ZodOptional<z.ZodString>;
 export declare const zStringParam: z.ZodString;
 export declare const zEnumQuery: <const T extends util.EnumLike>(entries: T) => z.ZodOptional<z.ZodEnum<T>>;
-export declare const zIntArrayQuery: z.ZodPipe<z.ZodOptional<z.ZodString>, z.ZodTransform<number[] | undefined, string | undefined>>;
+export declare const zIntArrayQuery: z.ZodOptional<z.ZodPipe<z.ZodString, z.ZodTransform<number[], string>>>;
 //# sourceMappingURL=types.d.ts.map
