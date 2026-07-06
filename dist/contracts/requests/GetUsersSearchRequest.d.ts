f2caea8ad858e5a6ab5f48db1c37e77e72aab492
Automated Publisher <actions@users.noreply.github.com>
2026-07-06 23:51:13 +0000

    Automated publish: Mon Jul  6 23:51:13 UTC 2026 a0ca20c27e76ae11aaec96c729e7d0cb9c775806

--- dist/contracts/requests/GetUsersSearchRequest.d.ts
+++ dist/contracts/requests/GetUsersSearchRequest.d.ts
@@ -6,11 +6,11 @@ export declare const zGetUsersSearchRequest: z.ZodObject<{
     sortField: z.ZodOptional<z.ZodString>;
     query: z.ZodOptional<z.ZodString>;
     sort: z.ZodDefault<z.ZodOptional<z.ZodEnum<typeof import("./SearchRequest.js").SortDirection>>>;
-    roleIds: z.ZodOptional<z.ZodPipe<z.ZodString, z.ZodTransform<number[], string>>>;
-    withDiscordUsers: z.ZodPipe<z.ZodPipe<z.ZodOptional<z.ZodEnum<{
+    isDonor: z.ZodPipe<z.ZodPipe<z.ZodOptional<z.ZodEnum<{
         true: "true";
         false: "false";
     }>>, z.ZodTransform<boolean, "true" | "false" | undefined>>, z.ZodBoolean>;
+    roleIds: z.ZodOptional<z.ZodPipe<z.ZodString, z.ZodTransform<number[], string>>>;
 }, z.core.$strip>;
 export type GetUsersSearchRequest = z.infer<typeof zGetUsersSearchRequest>;
 //# sourceMappingURL=GetUsersSearchRequest.d.ts.map
