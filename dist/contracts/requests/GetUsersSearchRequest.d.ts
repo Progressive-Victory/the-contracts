c554ac6fa7a58f590f70ef3c32317c8d8bb2c52e
Automated Publisher <actions@users.noreply.github.com>
2026-07-07 00:52:01 +0000

    Automated publish: Tue Jul  7 00:52:01 UTC 2026 1e1c17fd84d7a4d28a94247b387f5dee524e9fe3

--- dist/contracts/requests/GetUsersSearchRequest.d.ts
+++ dist/contracts/requests/GetUsersSearchRequest.d.ts
@@ -1,15 +1,15 @@
 import z from 'zod';
 export declare const zGetUsersSearchRequest: z.ZodObject<{
-    page: z.ZodPipe<z.ZodPipe<z.ZodOptional<z.ZodString>, z.ZodTransform<number | undefined, string | undefined>>, z.ZodOptional<z.ZodNumber>>;
-    limit: z.ZodDefault<z.ZodPipe<z.ZodPipe<z.ZodOptional<z.ZodString>, z.ZodTransform<number | undefined, string | undefined>>, z.ZodOptional<z.ZodNumber>>>;
+    page: z.ZodOptional<z.ZodPipe<z.ZodPipe<z.ZodString, z.ZodTransform<number, string>>, z.ZodNumber>>;
+    limit: z.ZodDefault<z.ZodOptional<z.ZodPipe<z.ZodPipe<z.ZodString, z.ZodTransform<number, string>>, z.ZodNumber>>>;
     searchField: z.ZodOptional<z.ZodString>;
     sortField: z.ZodOptional<z.ZodString>;
     query: z.ZodOptional<z.ZodString>;
     sort: z.ZodDefault<z.ZodOptional<z.ZodEnum<typeof import("./SearchRequest.js").SortDirection>>>;
-    isDonor: z.ZodPipe<z.ZodPipe<z.ZodOptional<z.ZodEnum<{
+    isDonor: z.ZodOptional<z.ZodPipe<z.ZodEnum<{
         true: "true";
         false: "false";
-    }>>, z.ZodTransform<boolean, "true" | "false" | undefined>>, z.ZodBoolean>;
+    }>, z.ZodTransform<boolean, "true" | "false">>>;
     roleIds: z.ZodOptional<z.ZodPipe<z.ZodString, z.ZodTransform<number[], string>>>;
 }, z.core.$strip>;
 export type GetUsersSearchRequest = z.infer<typeof zGetUsersSearchRequest>;
--- dist/util/types.d.ts
+++ dist/util/types.d.ts
@@ -1,10 +1,10 @@
 import z, { util } from 'zod';
 export declare const zIntParam: z.ZodPipe<z.ZodString, z.ZodTransform<number, string>>;
-export declare const zBoolQuery: z.ZodPipe<z.ZodPipe<z.ZodOptional<z.ZodEnum<{
+export declare const zBoolQuery: z.ZodOptional<z.ZodPipe<z.ZodEnum<{
     true: "true";
     false: "false";
-}>>, z.ZodTransform<boolean, "true" | "false" | undefined>>, z.ZodBoolean>;
-export declare const zIntQuery: z.ZodPipe<z.ZodPipe<z.ZodOptional<z.ZodString>, z.ZodTransform<number | undefined, string | undefined>>, z.ZodOptional<z.ZodNumber>>;
+}>, z.ZodTransform<boolean, "true" | "false">>>;
+export declare const zIntQuery: z.ZodOptional<z.ZodPipe<z.ZodPipe<z.ZodString, z.ZodTransform<number, string>>, z.ZodNumber>>;
 export declare const zStringQuery: z.ZodOptional<z.ZodString>;
 export declare const zStringParam: z.ZodString;
 export declare const zEnumQuery: <const T extends util.EnumLike>(entries: T) => z.ZodOptional<z.ZodEnum<T>>;
