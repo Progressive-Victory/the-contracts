d956816c0371891e455fe02a44be59a3dfca0dde
TristanCC <tristan.c.johnston@gmail.com>
2026-07-21 04:02:56 +0000

    remove stale dist files after rename

--- dist/contracts/data/DiscordSkillTeam.js
+++
@@ -1,8 +0,0 @@
-import z from 'zod';
-export const zDiscordSkillTeam = z.object({
-    teamRoleId: z.string().nonempty(),
-    teamName: z.string().nonempty(),
-    leadRoleId: z.string().nonempty(),
-    teamChannelId: z.string().nonempty(),
-});
-//# sourceMappingURL=DiscordSkillTeam.js.map
