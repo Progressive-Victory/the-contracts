8a0c19cfa71f2d2e851d8f37b01c947de77d03c0
zoe | bito <56496871+zoe2276@users.noreply.github.com>
2026-07-30 20:18:25 -0500

    the-api#215 Contract Updates (#31)
    * +UpdateDiscordUserRequest
    
    * Automated publish: Thu Jul 30 18:21:10 UTC 2026 aa9152c985ca0399ee87676d3f2fec4a5ee2a9ec
    
    * prettier
    
    this should really be done on the rest of the contracts too but that made the commit balloon
    
    * Automated publish: Thu Jul 30 18:32:11 UTC 2026 a4ecf11000b7804201fe9cff4b11a23ba2df6145
    
    ---------
    
    Co-authored-by: Automated Publisher <actions@users.noreply.github.com>

---
+++ dist/contracts/requests/UpdateDiscordUserRequest.js
@@ -0,0 +1,6 @@
+import { zCreateDiscordUserRequest } from './index.js';
+export const zUpdateDiscordUserRequest = zCreateDiscordUserRequest
+    .omit({ discordId: true })
+    .partial()
+    .strict();
+//# sourceMappingURL=UpdateDiscordUserRequest.js.map
