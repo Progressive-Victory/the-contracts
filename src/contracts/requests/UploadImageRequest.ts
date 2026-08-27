49bc2adf9f5395101bdf6acbe5bef5e60dc72a70
AmberLehmann <132320992+AmberLehmann@users.noreply.github.com>
2026-08-26 20:28:48 -0400

    Remove uneccesary fields

--- src/contracts/requests/UploadImageRequest.ts
+++ src/contracts/requests/UploadImageRequest.ts
@@ -1,23 +1,11 @@
-import { Readable } from 'node:stream';
 import z from 'zod';
-
-const MAX_FILE_SIZE = 50 * 1024 * 1024;
 
 const zAcceptedImageType = z.enum(['image/jpeg', 'image/png']);
 export type AcceptedImageType = z.infer<typeof zAcceptedImageType>;
 
 export const zUploadImageRequest = z.object({
-	file: z.instanceof(Readable, { error: 'You must upload an image' }),
 	filename: z.string(),
 	mimetype: zAcceptedImageType,
-	size: z
-		.number()
-		.int()
-		.positive()
-		.max(
-			MAX_FILE_SIZE,
-			`File must not exceed ${MAX_FILE_SIZE / 1024 / 1024}MB.`
-		),
 });
 
 export type UploadImageRequest = z.infer<typeof zUploadImageRequest>;
