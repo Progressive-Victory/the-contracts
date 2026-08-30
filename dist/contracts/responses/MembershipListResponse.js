470a4debb8172e193b48fadae90df0bd0bf0a86b
sH3llH0und <wyn@progress.win>
2026-08-30 09:38:29 -0600

    build

--- dist/contracts/responses/MembershipListResponse.js
+++ dist/contracts/responses/MembershipListResponse.js
@@ -1,13 +1,11 @@
 import { zActBlueDonor, zUser } from '../data/index.js';
 import z from 'zod';
-export const zMembershipListResponse = z.object({
-    memberships: z.array(z.object({
-        donor: zActBlueDonor,
-        customField: z.object({
-            label: z.string(),
-            answer: z.string()
-        }).nullable(),
-        user: zUser.nullable(),
-    })),
+export const zMembershipsResponsePacket = z.object({
+    donor: zActBlueDonor,
+    customField: z.object({
+        label: z.string(),
+        answer: z.string()
+    }).nullable(),
+    user: zUser.nullable(),
 });
 //# sourceMappingURL=MembershipListResponse.js.map
