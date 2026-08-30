e467dd37defd06b750ccff290b5bfe0bff402896
sH3llH0und <wyn@progress.win>
2026-08-30 09:37:56 -0600

    modified response structure

--- src/contracts/responses/MembershipListResponse.ts
+++ src/contracts/responses/MembershipListResponse.ts
@@ -1,17 +1,13 @@
 import { zActBlueDonor, zUser } from '../data/index.js';
 import z from 'zod';
 
-export const zMembershipListResponse = z.object({
-	memberships: z.array(
-		z.object({
-			donor: zActBlueDonor,
-            customField: z.object({
-                label: z.string(),
-                answer: z.string()
-            }).nullable(),
-			user: zUser.nullable(),
-		})
-	),
+export const zMembershipsResponsePacket = z.object({
+    donor: zActBlueDonor,
+    customField: z.object({
+        label: z.string(),
+        answer: z.string()
+    }).nullable(),
+    user: zUser.nullable(),
 });
 
-export type MembershipListResponse = z.infer<typeof zMembershipListResponse>
+export type MembershipsResponsePacket = z.infer<typeof zMembershipsResponsePacket>
