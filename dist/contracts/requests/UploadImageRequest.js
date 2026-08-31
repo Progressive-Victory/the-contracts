fe4913588aef9976ac7f1c46c97226ec0205e691
Automated Publisher <actions@users.noreply.github.com>
2026-08-31 03:58:55 +0000

    Automated publish: Mon Aug 31 03:58:55 UTC 2026 9a9edc70d62205a12e24d41be81b70f6c3b1050d

--- dist/contracts/requests/UploadImageRequest.js
+++ dist/contracts/requests/UploadImageRequest.js
@@ -1,5 +1,11 @@
 import z from 'zod';
 export const zUploadImageRequest = z.object({
-    image: z.custom(),
+    image: z.custom((val) => {
+        if (!val || typeof val !== 'object')
+            return false;
+        return 'file' in val && 'filename' in val && 'mimetype' in val;
+    }, {
+        message: 'Input must be a valid multipart file',
+    }),
 });
 //# sourceMappingURL=UploadImageRequest.js.map
