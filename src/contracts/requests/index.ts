abe4cd155a85841f1267da268f6e5d4a35584350
sH3llH0und <wyn@progress.win>
2026-09-02 13:21:14 -0600

    made the exports not retarded

--- src/contracts/requests/index.ts
+++ src/contracts/requests/index.ts
@@ -38,8 +38,3 @@ export * from './UpdateUserRequest.js';
 export * from './UploadImageRequest.js';
 export * from './UserOnboardingCollectInfoRequest.js';
 export * from './UserOnboardingVerifyRequest.js';
-
-import * as v2 from './v2/index.js'
-
-// eslint-disable-next-line eslint-plugin-index-file/enforce-index-usage
-export {v2}
