633047815ea5d6629c43fa161dddb33ef9bf9ab4
zoe2276 <zoe@zlynn.dev>
2026-09-07 18:16:33 +0000

    updates per leadership discussion

--- src/contracts/data/Endorsement.ts
+++ src/contracts/data/Endorsement.ts
@@ -1,46 +1,51 @@
 import z from 'zod';
 
 export enum InitiativeType {
-	State,
-	National,
+    State = 0,
+    National = 1,
 }
 export enum EndorsementType {
-	PVPledge,
-	Endorsement,
-	Recommendation,
+    PVPledge = 0,
+    Endorsement = 1,
+    Recommendation = 2,
+    Unendorsed = 3
 }
 export enum BackgroundColor {
-	Blue,
-	Yellow,
+    Blue = "blue",
+    Yellow = "yellow",
+    Red = "red"
 }
 export enum ElectionStatus {
-	NoElection,
-	UpcomingPrimary,
-	WonPrimary,
-	Elected,
-	LostPrimary,
-	LostGeneral,
-	DroppedOut,
+    NoElection = 0,
+    UpcomingPrimary = 1,
+    WonPrimary = 2,
+    Elected = 3,
+    LostPrimary = 4,
+    LostGeneral = 5,
+    DroppedOut = 6,
 }
 
 export const zEndorsement = z.object({
-	id: z.int(),
-	name: z.string(),
-	state: z.string(),
-	candidateLink: z.string(),
-	linkLabel: z.string(),
-	description: z.string(),
-	isStateInitiative: z.coerce.boolean(),
-	isNationalInitiative: z.coerce.boolean(),
-	isPvMember: z.coerce.boolean(),
-	tookPvPledge: z.coerce.boolean(),
-	imgUrl: z.string(),
-	primaryElection: z.coerce.date().nullable(),
-	generalElection: z.coerce.date().nullable(),
-	initiativeLevel: z.enum(InitiativeType),
-	endorsementLevel: z.enum(EndorsementType),
-	avatarBgColor: z.enum(BackgroundColor),
-	electionStatus: z.enum(ElectionStatus),
+    id: z.int(),
+    name: z.string(),
+    state: z.string(),
+    jurisiction: z.string().nullable(),
+    endorsementDate: z.coerce.date().nullable(),
+    endorsementReason: z.string().nullable(),
+    incumbent: z.boolean().nullable(),
+    handleHref: z.string().nullable(),
+    handle: z.string(),
+    quote: z.string(),
+    websiteHref: z.string(),
+    donateHref: z.string().nullable(),
+    isPvMember: z.boolean(),
+    imgUrl: z.string(),
+    primaryElection: z.coerce.date().nullable(),
+    generalElection: z.coerce.date().nullable(),
+    initiativeLevel: z.enum(InitiativeType),
+    endorsementLevel: z.enum(EndorsementType),
+    avatarBgColor: z.enum(BackgroundColor),
+    electionStatus: z.enum(ElectionStatus),
 });
 
 export type Endorsement = z.infer<typeof zEndorsement>;
