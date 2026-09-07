8668c8141f2a4db936d9d836344f548f6e1eb286
zoe2276 <zoe@zlynn.dev>
2026-09-07 18:42:45 +0000

    requisite updates to request contracts

--- src/contracts/requests/UpdateEndorsementRequest.ts
+++ src/contracts/requests/UpdateEndorsementRequest.ts
@@ -1,32 +1,36 @@
 import {
-	BackgroundColor,
-	ElectionStatus,
-	EndorsementType,
-	InitiativeType,
+    BackgroundColor,
+    ElectionStatus,
+    EndorsementType,
+    InitiativeType,
 } from '../data/index.js';
 import z from 'zod';
 
 // This is not meaningfully different than CreateEndorsementRequest which isn't meaningfully different than Endorsement
 // So edit all three
 export const zUpdateEndorsementRequest = z.object({
-	name: z.string().nonempty().max(100).optional(),
-	state: z.string().nonempty().max(36).optional(),
-	description: z.string().nonempty().max(300).optional(),
-	candidateLink: z.string().max(200).optional(),
-	linkLabel: z.string().max(50).optional(),
-	imgUrl: z.string().max(200).optional(),
-	isStateInitiative: z.boolean().optional(),
-	isNationalInitiative: z.boolean().optional(),
-	isPvMember: z.boolean().optional(),
-	tookPvPledge: z.boolean().optional(),
-	primaryElection: z.coerce.date().optional().nullable(),
-	generalElection: z.coerce.date().optional().nullable(),
-	initiativeLevel: z.enum(InitiativeType).optional(),
-	endorsementLevel: z.enum(EndorsementType).optional(),
-	avatarBgColor: z.enum(BackgroundColor).optional(),
-	electionStatus: z.enum(ElectionStatus).optional(),
+    name: z.string().nonempty().max(100).optional(),
+    state: z.string().nonempty().max(36).optional(),
+    jurisiction: z.string().nullish(),
+    endorsementDate: z.coerce.date().nullish(),
+    endorsementReason: z.string().nullish(),
+	publishEndorsement: z.boolean().optional(),
+	incumbent: z.boolean().optional(),
+    handleHref: z.string().max(200).nullish(),
+    handle: z.string().max(50).optional(),
+    quote: z.string().nonempty().max(300).optional(),
+    websiteHref: z.string().optional(),
+    donateHref: z.string().nullish(),
+    imgUrl: z.string().max(200).optional(),
+    isPvMember: z.boolean().optional(),
+    primaryElection: z.coerce.date().nullish(),
+    generalElection: z.coerce.date().nullish(),
+    initiativeLevel: z.enum(InitiativeType).optional(),
+    endorsementLevel: z.enum(EndorsementType).optional(),
+    avatarBgColor: z.enum(BackgroundColor).optional(),
+    electionStatus: z.enum(ElectionStatus).optional(),
 });
 
 export type UpdateEndorsementRequest = z.infer<
-	typeof zUpdateEndorsementRequest
+    typeof zUpdateEndorsementRequest
 >;
