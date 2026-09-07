67d383743cd872494606d2a1af4d1d5aec2154a2
Automated Publisher <actions@users.noreply.github.com>
2026-09-07 18:55:28 +0000

    Automated publish: Mon Sep  7 18:55:28 UTC 2026 be215888797140051a54247b41595e3a34a948e7

--- dist/contracts/data/Endorsement.d.ts
+++ dist/contracts/data/Endorsement.d.ts
@@ -10,9 +10,9 @@ export declare enum EndorsementType {
     Unendorsed = 3
 }
 export declare enum BackgroundColor {
-    Blue = "blue",
-    Yellow = "yellow",
-    Red = "red"
+    Blue = 0,
+    Yellow = 1,
+    Red = 2
 }
 export declare enum ElectionStatus {
     NoElection = 0,
