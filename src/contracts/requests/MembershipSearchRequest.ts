f636c6571b4a54b0fd154237691007a0f7bbd07f
sH3llH0und <wyn@progress.win>
2026-09-17 17:09:49 -0600

    made bools nullable

--- src/contracts/requests/MembershipSearchRequest.ts
+++ src/contracts/requests/MembershipSearchRequest.ts
@@ -2,8 +2,8 @@ import z from 'zod'
 import { zSearchRequest } from './SearchRequest.js'
 
 export const zMembershipSearchRequest = zSearchRequest.extend({
-    isBenefitEligible: z.boolean(),
-    isMember: z.boolean()
+    isBenefitEligible: z.boolean().nullable(),
+    isMember: z.boolean().nullable()
 })
 
 export type MembershipSearchRequest = z.infer<typeof zMembershipSearchRequest>
