33ea8f30fd8ef154e32ddddbe94099b731d04f59
sH3llH0und <wyn@progress.win>
2026-09-17 18:04:38 -0600

    holy fucking shit im retarded use bololdwlad query

--- src/contracts/requests/MembershipSearchRequest.ts
+++ src/contracts/requests/MembershipSearchRequest.ts
@@ -1,9 +1,10 @@
 import z from 'zod'
 import { zSearchRequest } from './SearchRequest.js'
+import { zBoolQuery } from '../../util/types.js'
 
 export const zMembershipSearchRequest = zSearchRequest.extend({
-    isBenefitEligible: z.boolean().nullable(),
-    isMember: z.boolean().nullable()
+    isBenefitEligible: zBoolQuery,
+    isMember: zBoolQuery
 })
 
 export type MembershipSearchRequest = z.infer<typeof zMembershipSearchRequest>
