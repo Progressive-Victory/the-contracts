758f142d01aa6fbcca9c4230c75d58d670427aea
sH3llH0und <wyn@progress.win>
2026-09-21 17:44:27 -0600

    position_changes

--- src/contracts/data/Position.ts
+++ src/contracts/data/Position.ts
@@ -1,10 +1,25 @@
 import z from 'zod';
 
+export enum PositionTypes{
+	POSITION = 0,
+	GROUP = 1
+}
+
+export enum RelationshipTypes {
+	OWNER = 0
+}
+
+export const zPositionTypes = z.enum(PositionTypes)
+
+export const zRelationalTypes = z.enum(RelationshipTypes)
+
 export const zPosition = z.object({
 	id: z.int(),
 	name: z.string(),
 	childIds: z.array(z.int()),
 	userIds: z.array(z.int()),
+	type: zPositionTypes,
+	seats: z.int()
 });
 
 export type Position = z.infer<typeof zPosition>;
